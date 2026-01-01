26ce978fb65cf61d065dcc3ca8e9d2f789724004
ramazan_polat <ramazanpolat02@gmail.com>
2026-01-01 22:07:25 +0300

    sql files created for dbt

---
+++ airbnb/models/src/src_reviews.sql
@@ -0,0 +1,4 @@
+with raw_reviews as (select *
+                     from airbnb.raw.raw_reviews)
+select listing_id, date as review_date, comments as review_text, reviewer_name, sentiment as review_sentiment
+from raw_reviews
