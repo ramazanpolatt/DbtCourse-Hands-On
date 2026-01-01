b8df3169e5ee8d36e68d91779efc3ccb7160ac9c
ramazan_polat <ramazanpolat02@gmail.com>
2026-01-01 22:42:59 +0300

    fullmoon mart created and sources.yml added

--- airbnb/models/src/src_hosts.sql
+++ airbnb/models/src/src_hosts.sql
@@ -1,4 +1,4 @@
-with raw_hosts as (select * from airbnb.raw.raw_hosts)
+with raw_hosts as (select * from {{source('airbnb', 'hosts')}})
 select id   as host_id,
        name as host_name,
        is_superhost,
